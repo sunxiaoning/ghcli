e0e42c57f926f04709d9e360a07e8862e8eac36c
sunxiaoning <1530358579@qq.com>
2024-10-15 13:55:46 +0800

    feat: optimize github cli download speed
    - added support for using gitee repository as an origin.

--- autorun.sh
+++ autorun.sh
@@ -8,15 +8,22 @@ GIT_VERSION=${GIT_VERSION:-"2.43.5"}
 GH_NAME=${GH_NAME:-"gh"}
 GH_VERSION=${GH_VERSION:-"2.53.0"}
 ARCH=${ARCH:-"amd64"}
-RPM_GH_URL=https://github.com/cli/cli/releases/download/v${GH_VERSION}/${GH_NAME}_${GH_VERSION}_linux_${ARCH}.rpm
+
+REPO_ORIGIN_SOURCE=${REPO_ORIGIN_SOURCE:-"0"}
+
+RPM_GH_GITHUB_REPO="github.com/cli/cli"
+RPM_GH_GITEE_REPO="gitee.com/williamsun"
+
+RPM_GH_REPO=${RPM_GH_REPO:-${RPM_GH_GITHUB_REPO}}
 
 install-git() {
   echo "Install git ..."
-  if ! rpm -q "git-${GIT_VERSION}" &> /dev/null; then
+
+  if ! rpm -q "git-${GIT_VERSION}" &>/dev/null; then
     yum -y install "git-${GIT_VERSION}"
   fi
-  
-  if ! rpm -q "git-${GIT_VERSION}" &> /dev/null; then
+
+  if ! rpm -q "git-${GIT_VERSION}" &>/dev/null; then
     echo "Install git-${GIT_VERSION} failed!" >&2
     exit 1
   fi
@@ -24,13 +31,18 @@ install-git() {
 
 install-gh() {
   echo "Install gh ..."
-  if ! rpm -q "${GH_NAME}-${GH_VERSION}" &> /dev/null; then
-    curl -Lo "/tmp/${GH_NAME}_${GH_VERSION}_linux_${ARCH}.rpm" "${RPM_GH_URL}"
-    yum -y install /tmp/${GH_NAME}_${GH_VERSION}_linux_${ARCH}.rpm
-    # rm -f "/tmp/${GH_NAME}_${GH_VERSION}_linux_${ARCH}.rpm"
+
+  if [[ "1" == "${REPO_ORIGIN_SOURCE}" ]]; then
+    RPM_GH_REPO="${RPM_GH_GITEE_REPO}"
   fi
 
-  if ! rpm -q "${GH_NAME}-${GH_VERSION}" &> /dev/null; then
+  if ! rpm -q "${GH_NAME}-${GH_VERSION}" &>/dev/null; then
+    curl -fsSLo "/tmp/${GH_NAME}_${GH_VERSION}_linux_${ARCH}.rpm" "https://${RPM_GH_REPO}/releases/download/v${GH_VERSION}/${GH_NAME}_${GH_VERSION}_linux_${ARCH}.rpm"
+    yum -y install /tmp/${GH_NAME}_${GH_VERSION}_linux_${ARCH}.rpm
+    rm -f "/tmp/${GH_NAME}_${GH_VERSION}_linux_${ARCH}.rpm"
+  fi
+
+  if ! rpm -q "${GH_NAME}-${GH_VERSION}" &>/dev/null; then
     echo "Install ${GH_NAME}-${GH_VERSION} failed!" >&2
     exit 1
   fi
@@ -39,7 +51,7 @@ install-gh() {
 auth-gh() {
   echo "Auth login gh ..."
   if gh auth status &>/dev/null; then
-    exit 0
+    return 0
   else
     local gh_token=${GH_TOKEN:-""}
     unset GH_TOKEN
@@ -63,13 +75,12 @@ auth-gh() {
   fi
 }
 
-
 main() {
 
-    # Here, Git is manually installed to specify the desired Git version. In fact, when gh is installed, Git will be automatically installed.
-    install-git
-    install-gh
-    auth-gh
+  # Here, Git is manually installed to specify the desired Git version. In fact, when gh is installed, Git will be automatically installed.
+  install-git
+  install-gh
+  auth-gh
 }
 
 main "$@"
